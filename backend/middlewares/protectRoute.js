2332b48082a4d6735df6ee3c2055ed5850920a62
Hasan36arnob <arnobhasanice@gmail.com>
2026-03-25 12:04:14 +0600

    11

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -6,18 +6,22 @@ const protectRoute = async (req, res, next) => {
 	try {
 		const token = req.cookies.jwt;
 
-		if (!token) return res.status(401).json({ message: "Unauthorized" });
+		if (!token) return res.status(401).json({ error: "Unauthorized" });
 
 		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
 		const user = await User.findById(decoded.userId).select("-password");
 
+		if (!user) {
+			return res.status(401).json({ error: "User not found" });
+		}
+
 		req.user = user;
 
 		next();
 	} catch (err) {
-		res.status(500).json({ message: err.message });
-		console.log("Error in signupUser: ", err.message);
+		res.status(401).json({ error: "Unauthorized: Invalid token" });
+		console.log("Error in protectRoute: ", err.message);
 	}
 };
 
